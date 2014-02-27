bc68971684bf5595424dd2aa17851537d35ffc09
Tommy Nguyen <tommy.nguyen@eastagile.com>
2014-02-28 02:55:32 +0700

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,55 +1,30 @@
 require 'spec_helper'
 
-titleMessage ="Ruby on Rails Tutorial Sample App |"
-describe "Static Pages" do
-  describe "Home page" do
-      it "should have the content 'Sample App'" do
-      	visit '/static_pages/home'
-      	expect(page).to have_content ('Sample App')
-      end
-      it "should have the title 'Home'" do
-	      visit '/static_pages/home'
-	      expect(page).to have_title(titleMessage)
-      end 
-      it "should not have the title 'Home'" do
-	      visit '/static_pages/home'
-	      expect(page).not_to have_title("| Home")
-      end 
-  end
-
-  describe "Help page" do
-  	it "should have the content 'Help'" do
-  		visit '/static_pages/help'
-  		expect(page).to have_content ('Help')
-  	end
-	 it "should have the title 'Help'" do
-	      visit '/static_pages/help'
-	      expect(page).to have_title(titleMessage+" Help")
-	 end
-  end
-
-  describe "About page" do
-	  it "should have the content 'About Us'" do
-		  visit '/static_pages/about'
-		  expect(page).to have_content ('About Us')
-	  end
-	  it "should have the title 'About Us'" do
-		  visit '/static_pages/about'
-		  expect(page).to have_title (titleMessage+" About Us")
-	  end
-	
-  end
-  describe "Contact page" do
-	  it "should have the content 'Contact'" do
-		  visit '/static_pages/contact'
-		  expect(page).to have_content ('Contact')
-	  end
-	  it "should have the title 'Contact'" do
-		  visit '/static_pages/contact'
-		  expect(page).to have_title ("Ruby on Rails Tutorial Sample App | Contact")
+	titleMessage ="Ruby on Rails Tutorial Sample App |"
+	describe "Static Pages" do
+	  subject {page}
+	  describe "Home page" do
+	  	before {visit root_path}
+	      it { should have_content ('Sample App')}
+	      tempStr = titleMessage.gsub(" |","")
+	      it {should have_title(tempStr)}
+	      it {should_not have_title("| Home")}
 	  end
 
- end
+	  describe "Help page" do
+	  	before {visit help_path}
+	  	it {should have_content ('Help')}
+		it {should have_title(titleMessage+" Help")}
+	  end
 
-
+	  describe "About page" do
+	  	  before{visit about_path}
+		  it {should have_content ('About Us')}
+		  it {should have_title (titleMessage+" About Us")}
+	  end
+	  describe "Contact page" do
+	  	  before{visit contact_path}
+		  it {should have_content ('Contact')}
+		  it {should have_title (titleMessage+" Contact")}
+	end
 end
