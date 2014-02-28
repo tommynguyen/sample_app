bdae702ed747531f8b1c244bf8988787e4004fbd
Tommy Nguyen <tommy.nguyen@eastagile.com>
2014-02-28 09:45:20 +0700

    Fixed utilities.rb bug

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,30 +1,44 @@
 require 'spec_helper'
-
 	titleMessage ="Ruby on Rails Tutorial Sample App |"
 	describe "Static Pages" do
 	  subject {page}
 	  describe "Home page" do
 	  	before {visit root_path}
 	      it { should have_content ('Sample App')}
-	      tempStr = titleMessage.gsub(" |","")
-	      it {should have_title(tempStr)}
-	      it {should_not have_title("| Home")}
-	  end
+	      it {should have_title(full_title(''))}
+	      it {should_not have_title(full_title("Home"))}
+	end
 
-	  describe "Help page" do
+	describe "Help page" do
 	  	before {visit help_path}
 	  	it {should have_content ('Help')}
-		it {should have_title(titleMessage+" Help")}
-	  end
+		it {should have_title(full_title("Help"))}
+	end
 
-	  describe "About page" do
-	  	  before{visit about_path}
-		  it {should have_content ('About Us')}
-		  it {should have_title (titleMessage+" About Us")}
-	  end
-	  describe "Contact page" do
-	  	  before{visit contact_path}
-		  it {should have_content ('Contact')}
-		  it {should have_title (titleMessage+" Contact")}
+	describe "About page" do
+	  	before{visit about_path}
+		it {should have_content ('About Us')}
+		it {should have_title (full_title("About Us"))}
+	end
+	describe "Contact page" do
+	  	before{visit contact_path}
+		it {should have_content ('Contact')}
+		it {should have_title (full_title("Contact"))}
+	end
+	describe "Link for layouts" do
+		it "should have the right links on the layout" do
+			visit root_path
+			click_link "About"
+			expect(page).to have_title(full_title('About Us'))
+			click_link "Help"
+			expect(page).to have_title(full_title('Help'))
+			click_link "Contact"
+			expect(page).to have_title(full_title('Contact'))
+			click_link "Home"
+			click_link "Sign up now!"
+			expect(page).to have_title(full_title('Sign up'))
+			click_link "sample app"
+			expect(page).to have_title(full_title('Sign up'))
+		end
 	end
 end
